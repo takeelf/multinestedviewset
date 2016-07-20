2eca36ae700bf7cacf881ba5830d440ac0a7ce07
wonho <wonho@dibiup.com>
2016-07-20 14:09:32 +0900

    Init

--- nestedthing/models.py
+++
@@ -1,8 +0,0 @@
-from __future__ import unicode_literals
-
-from django.db import models
-
-
-class NestedThing(models.Model):
-    user = models.ForeignKey('auth.User', related_name='nested')
-    nested_name = models.CharField(max_length=30)
